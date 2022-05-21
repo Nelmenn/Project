e71a3624ae2713e569e3c723e6f80e8e3ee25cb6
Nelmenn <pyha43@gmail.com>
2022-05-21 19:48:57 +0500

    Update 2.0

---
+++ static/js/vue.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        items: [
+            'Home',
+            'Home',
+            'Home',
+            'Home',
+        ],
+        show: false,
+    },
+});
